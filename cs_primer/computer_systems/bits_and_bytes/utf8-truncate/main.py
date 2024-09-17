3e47cc5e23f5b8b0bf9756352d31ce856c4694df
John Hannebery <johnhannebery@gmail.com>
2024-09-17 16:24:28 +1000

    push up to sneaky nan

---
+++ cs_primer/computer_systems/bits_and_bytes/utf8-truncate/main.py
@@ -0,0 +1,21 @@
+with open("cases", "rb") as f:
+    transformed = b""
+    for line in f:
+        # drop the trailing newline
+        line = line[:-1]
+
+        trunc_length = int(line[0])
+
+        # remove the n_bytes to truncate
+        line = line[1:]
+
+        if trunc_length < len(line):
+            while line[trunc_length] & 0xC0 == 0x80:
+                trunc_length -= 1
+
+        newline = line[:trunc_length]
+
+        transformed += newline + b"\x0a"
+
+with open("truncated", "wb") as f:
+    f.write(transformed)
