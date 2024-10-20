c989483f23bc934e86000dc25cd9547b713d1fde
John Hannebery <johnhannebery@gmail.com>
2024-10-20 21:59:08 +1100

    ud

--- cs_primer/computer_networks/http/proxy-persistent/main.py
+++ cs_primer/computer_networks/http/proxy-persistent/main.py
@@ -18,19 +18,27 @@ if __name__ == "__main__":
     proxy_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     proxy_socket.bind(OWN_ADDRESS)
 
-    proxy_socket.listen()  # only one connection at a time
+    proxy_socket.listen(1)  # only one connection at a time
     logger.info(f"Listening for connections on {OWN_ADDRESS}")
     keepalive = False
 
-    while True:
-        try:  # avoid any errors that can crash your server, want it long running
-            # while True:
-            client_socket, client_address = proxy_socket.accept()
+    # while True:
+    try:  # avoid any errors that can crash your server, want it long running
+        while True:
+            if not keepalive:
+                client_socket, client_address = proxy_socket.accept()
 
-            logger.info(f"Accepted connection with address {client_address}")
+                logger.info(
+                    f"Accepted connection with address {client_address}"
+                )
 
             incoming_data = client_socket.recv(4096)
 
+            if not incoming_data:
+                logger.debug("No more data from client")
+                break
+
+            logger.debug(f"Received {len(incoming_data)} bytes from client")
             req_headers, req_body = incoming_data.split(b"\r\n\r\n", 1)
 
             req, headers = req_headers.split(b"\r\n", 1)
@@ -45,7 +53,7 @@ if __name__ == "__main__":
                         keepalive = False
 
             logger.debug(
-                f"Received request {method} {path} {protocol} with Connection: {connection}, total {len(incoming_data)} bytes from client"
+                f"Received request {req} with Connection: {connection}, total {len(incoming_data)} bytes from client"
             )
 
             upstream_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -56,23 +64,25 @@ if __name__ == "__main__":
 
             response_to_proxy = b""
 
-            while True:  # need to loop as packets might be split, in the case of response bodies governed by the content length header
-                # curl (18): transfer closed with outstanding read data remaining
-                # knows that server has to send content but connection is closed if we don't loop
+            while True:
                 server_response_bytechunk = upstream_socket.recv(4096)
                 logger.debug(
                     f"Received {len(server_response_bytechunk)} bytes from server"
                 )
                 response_to_proxy += server_response_bytechunk
                 if not server_response_bytechunk:
+                    response_to_proxy = response_to_proxy.replace(
+                        b"HTTP/1.0", b"HTTP/1.1"
+                    )
+                    logger.debug(
+                        "Modified response to client from HTTP/1.0 to HTTP/1.1"
+                    )
+                    logger.debug(response_to_proxy[:250])
                     client_socket.send(response_to_proxy)
                     logger.debug(
                         f"Sent {len(response_to_proxy)} bytes to client from server"
                     )
                     break
-
-            # side note; one can open sockets in a context manager, with socket.socket() as s: as it is a file descriptor like object
-            # that way, the socket is closed automatically when the block is exited
 
             # logger.debug(
             #     f"Closing client connection with address {client_address}"
@@ -87,30 +97,34 @@ if __name__ == "__main__":
                     f"Closing client connection with address {client_address}"
                 )
 
-        except (
-            ConnectionRefusedError
-        ):  # if the server is down, we can't connect to it
-            logger.error("Connection to server refused")
-            resp_body = b"<html><body><h1>502 Bad Gateway</h1></body></html>"
-            resp_req_headers = f"HTTP/1.1 502 Bad Gateway\r\nContent-Type: text/html\r\nContent-Length: {len(resp_body)}\r\n\r\n".encode(
-                "utf-8"
-            )
-            client_socket.send(resp_req_headers + resp_body)
+    except (
+        ConnectionRefusedError
+    ):  # if the server is down, we can't connect to it
+        logger.error("Connection to server refused")
+        resp_body = b"<html><body><h1>502 Bad Gateway</h1></body></html>"
+        resp_req_headers = f"HTTP/1.1 502 Bad Gateway\r\nContent-Type: text/html\r\nContent-Length: {len(resp_body)}\r\n\r\n".encode(
+            "utf-8"
+        )
+        client_socket.send(resp_req_headers + resp_body)
 
-        except OSError as e:
-            logger.error(f"An error occurred: {e}")
-            resp_body = (
-                b"<html><body><h1>500 Internal Server Error</h1></body></html>"
-            )
-            resp_req_headers = f"HTTP/1.1 500 Internal Server Error\r\nContent-Type: text/html\r\nContent-Length: {len(resp_body)}\r\n\r\n".encode(
-                "utf-8"
-            )
-            client_socket.send(resp_req_headers + resp_body)
+    except OSError as e:
+        logger.exception(f"An error occurred: {e}")
+        resp_body = (
+            b"<html><body><h1>500 Internal Server Error</h1></body></html>"
+        )
+        resp_req_headers = f"HTTP/1.1 500 Internal Server Error\r\nContent-Type: text/html\r\nContent-Length: {len(resp_body)}\r\n\r\n".encode(
+            "utf-8"
+        )
+        client_socket.send(resp_req_headers + resp_body)
 
-        finally:
-            if upstream_socket:
-                upstream_socket.close()
-            if client_socket:
-                client_socket.close()
+    finally:
+        if upstream_socket:
+            upstream_socket.close()
+            logger.debug("Closed connection with server")
+        if client_socket:
+            client_socket.close()
+            logger.debug(
+                f"Closing client connection with address {client_address}"
+            )
 
     proxy_socket.close()
